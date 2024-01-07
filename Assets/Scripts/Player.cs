be465acfdc898b5179c6f08a5242f42d08ba9cd2
Scott Burgess <burgess.scott91@gmail.com>
2024-01-07 00:53:07 -0500

    Update Player.cs
    Need to fix an issue where player cannot jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // get reference to rigidbody
     private Rigidbody2D _rigidbody;
-    // variable for jumpForce
+    [SerializeField]
+    private float _jumpForce = 5.0f;
+    [SerializeField]
+    private bool _isGrounded = false;
+
+    [SerializeField]
+    private LayerMask _groundLayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        // assign handle of rb here
         _rigidbody = GetComponent<Rigidbody2D>();
-
     }
 
     // Update is called once per frame
@@ -27,8 +30,21 @@ public class Player : MonoBehaviour
         
         float move = Input.GetAxisRaw("Horizontal");
 
-        // if space key && isGrounded == true
-        // current velocity = new velocity (current x, jumpForce)
+        
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded == true)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpForce);
+            _isGrounded = false;
+        }
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, _groundLayer.value);
+        Debug.DrawRay(transform.position, Vector2.down, Color.green);
+
+        if (hitInfo.collider != null)
+        {
+            Debug.Log("Hit: " + hitInfo.collider.name);
+            _isGrounded = true;
+        }
         
         _rigidbody.velocity = new Vector2(move, _rigidbody.velocity.y);
     }
